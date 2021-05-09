9d8deda5c399efa13418f9566ce0ed522a80c369
Supreeth Chandrashekhar <supreeth.chandrashekhar@philips.com>
2021-05-09 14:28:46 +0530

    Array Category Problem : two number sum

---
+++ arrays/two_number_sum.py
@@ -0,0 +1,9 @@
+"""
+Rule : You can't add a single integer to itself in order to obtain the traget sum
+    Background: There will be atmost one pair of numbers summing up to the traget sum.
+
+- Given: A non-empty array of distinct integers.
+- When : Any two numbers in the input array sum up to target sum
+- Then:  The writen function should return the numbers in an array of any order.
+- But : No Numbers sum up to the target sum, the function should return empty array
+"""
