5cce47f104237b4d7f657e4e75078e023e0f4352
Supreeth Chandrashekhar <supreeth.chandrashekhar@philips.com>
2021-05-15 23:37:49 +0530

    find the sum of two values using hashmap with documentation

--- arrays/two_number_hash_table.py
+++ arrays/two_number_hash_table.py
@@ -10,18 +10,37 @@ Rule : You can't add a single integer to itself in order to obtain the traget su
 
 
 def twoNumberSum(array, targetSum):
-    nums = {}
+    """
+    The problem comes down to this : targetsum = x+y 
+    So, since I know the tragetsum, I am trying ot find out the value of x 
+    which can be formed into a equation 'x = targetsum-y'
+    
+    
+    What really happens lets explain with an example 
+    
+    array = [3,7,1,9]
+    
+    During the first iteration, when the iteration is 3, then potential match is to be 7. 
+    However, it is seen the dict does'nt have 7 yet. During the next iteration you would
+    get potential value to be as 3 that is when the target sum is made. 
+    
+    However the limitation of the problem is it will return the first match. If there multiple matches then it would not.
+    
+    """
+    nums={}
     for num in array:
-        potential_match = targetSum - num
-        if potential_match in nums:
-            return [potential_match, num]
+        potentialMatch = targetSum-num
+        if potentialMatch in nums:
+            return [potentialMatch, num]
         else:
-            nums[num]= True
+            nums[num] = True
     return []
-
+    
+    
 
 if __name__ == '__main__':
-    array = [3,5,-4, 8, 11, 1, -1, 6]
+    #array = [3,5,-4, 8, 11, 1, -1, 6]
+    array = [3,7,-4, 8, 11, 1, -1, 6]
     targetsum = 10
     output = twoNumberSum(array, targetsum)
     print(output)
